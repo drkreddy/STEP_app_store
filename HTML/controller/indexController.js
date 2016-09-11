cc5e8509edb1b4a2a35dafbe0d8af866860bf307
sumanmaity112 <suman.maity112@gmail.com>
2016-09-11 15:30:29 +0530

    Suman | Added details pop up for a project

--- HTML/controller/indexController.js
+++ HTML/controller/indexController.js
@@ -1,16 +1,22 @@
-StepAppStore.entrance.controller("StepAppStore.entrance.index",['$scope', '$http', 'Projects', function ($scope, $http, Projects) {
+StepAppStore.entrance.controller("StepAppStore.entrance.index", ['$scope', '$http', 'Projects', function ($scope, $http, Projects) {
     $http.get("/loginAs").then(function (res) {
         $scope.loginName = res.data.name
     });
     $scope.projects = [];
     Projects.all().then(function (projects) {
         projects.forEach(function (project) {
-           project.uploadedon = moment(new Date(project.uploadedon).toISOString()).tz('Asia/Kolkata').format('DD-MM-YYYY hh:mma')
+            project.uploadedon = moment(new Date(project.uploadedon).toISOString()).tz('Asia/Kolkata').format('DD-MM-YYYY hh:mma');
         });
         $scope.projects = projects;
     });
-    $.get("/personalDetails",function(data){
+    $.get("/personalDetails", function (data) {
         $scope.isLoggedIn = data.isLoggedIn;
         $scope.username = data.username;
     });
+    $scope.getDetails = function (uuid) {
+        Projects.getDetails(uuid).then(function (details) {
+            details.uploadedon = moment(new Date(details.uploadedon).toISOString()).tz('Asia/Kolkata').format('DD-MM-YYYY hh:mma');
+            $scope.details = details;
+        });
+    }
 }]);
--- HTML/factories/indexFactory.js
+++ HTML/factories/indexFactory.js
@@ -1,12 +1,17 @@
 'use strict';
 
 angular.module('StepAppStore.entrance')
-    .factory('Projects', function($http){
+    .factory('Projects', function ($http) {
         return {
-            all: function(){
-                return $http.get("/getAllProjects").then(function(res){
+            all: function () {
+                return $http.get("/getAllProjects").then(function (res) {
                     return res.data;
 
+                })
+            },
+            getDetails: function (uuid) {
+                return $http.get("/project/" + uuid).then(function (res) {
+                    return res.data[0];
                 })
             }
         };
