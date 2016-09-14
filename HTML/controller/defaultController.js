7d64554d0a6564dbf6cb2753f74ed087f8a1701a
sumanmaity112 <suman.maity112@gmail.com>
2016-09-14 21:46:22 +0530

    Suman | Added Logout button to header

--- HTML/controller/defaultController.js
+++ HTML/controller/defaultController.js
@@ -1,5 +1,6 @@
 StepAppStore.default.controller("StepAppStore.default", ['$scope', '$http', function ($scope, $http) {
     $http.get("/personalDetails").then(function (res) {
         $scope.username = res.data.username;
+        $scope.isLoggedIn = res.data.isLoggedIn;
     });
 }]);
